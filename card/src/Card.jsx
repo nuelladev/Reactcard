ebf252821ec95d4941bc9fc5742e2ff11a970dc8
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-31 13:02:41 +0100

    initial commit

---
+++ card/src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card() {
+  return (
+    <div className="card">
+      <img src="https://media.licdn.com/dms/image/C4E03AQFnY9ZWFfH1mA/profile-displayphoto-shrink_400_400/0/1617965015883?e=1712188800&v=beta&t=NpilcMZTlVaDyjWQRKxbkmLwIfdfLaaKASJ5ZdCDZIc" alt="profile-pic" />
+      <h2>Emmanuella Okafor</h2>
+      <p>Passonate software and devops engineer</p>
+    </div>
+  )
+}
+
+export default Card
